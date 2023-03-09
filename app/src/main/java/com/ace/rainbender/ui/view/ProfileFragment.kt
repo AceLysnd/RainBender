2802f6567a399f1fdbda300fcccbac36b82f0744
aceilham <ilhamakbar.pradana@gmail.com>
2023-03-09 11:32:21 +0700

    Added update user data

--- app/src/main/java/com/ace/rainbender/ui/view/ProfileFragment.kt
+++ app/src/main/java/com/ace/rainbender/ui/view/ProfileFragment.kt
@@ -3,16 +3,20 @@ package com.ace.rainbender.ui.view
 import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
 import android.util.Base64
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.viewModels
 import coil.load
 import coil.transform.RoundedCornersTransformation
 import com.ace.rainbender.R
+import com.ace.rainbender.data.local.user.AccountEntity
 import com.ace.rainbender.databinding.FragmentNewsBinding
 import com.ace.rainbender.databinding.FragmentProfileBinding
 import com.ace.rainbender.ui.viewmodel.HomeFragmentViewModel
@@ -42,12 +46,12 @@ class ProfileFragment : Fragment() {
     }
 
     private fun loadProfile() {
-        viewModel.getAccountId().observe(viewLifecycleOwner){
+        viewModel.getAccountId().observe(viewLifecycleOwner) {
             viewModel.getAccountById(it)
-            viewModel.profileData.observe(viewLifecycleOwner){
+            viewModel.profileData.observe(viewLifecycleOwner) {
                 binding.etUsername.setText(it.username)
                 binding.etEmail.setText(it.email)
-                if (it.profilePicture != null){
+                if (it.profilePicture != null) {
                     setProfilePicture(convertStringToBitmap(it.profilePicture!!))
                 }
             }
@@ -70,10 +74,38 @@ class ProfileFragment : Fragment() {
     }
 
     private fun setOnclickListeners() {
-        binding.btnLogout.setOnClickListener{
+        binding.btnLogout.setOnClickListener {
             viewModel.setLoginStatus(false)
             val intent = Intent(requireActivity(), MainActivity::class.java)
             startActivity(intent)
+        }
+
+        binding.btnUpdateProfile.setOnClickListener {
+            AlertDialog.Builder(requireActivity())
+                .setTitle("Update Profile")
+                .setMessage("Are you sure to update profile?")
+                .setPositiveButton(
+                    "Yep"
+                ) { _, _ ->
+                    updateProfile()
+                }
+                .setNegativeButton("Cancel") { dialog, _ -> dialog.cancel() }
+                .show()
+//            updateProfile()
+        }
+    }
+
+    private fun updateProfile() {
+        viewModel.profileData.observe(viewLifecycleOwner) {
+            val accounEntity = AccountEntity(
+                accountId = it.accountId,
+                username = binding.etUsername.text.toString(),
+                email = binding.etEmail.text.toString(),
+                password = it.password,
+                profilePicture = it.profilePicture,
+                bookmark = it.bookmark
+            )
+            viewModel.updateUser(accounEntity)
         }
     }
 
